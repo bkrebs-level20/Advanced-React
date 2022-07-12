8261ddae51668aaf36e4a5faa73c734fdfbbdc46
bkrebs-level20 <bkrebs@level20.com>
2022-07-12 11:41:36 -0400

    Lesson 14 - Complete

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,5 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import nProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 // eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
