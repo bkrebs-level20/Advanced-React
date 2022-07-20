ed44e14841eed5ca15c07095921cb9b310ba1e8c
bkrebs-level20 <bkrebs@level20.com>
2022-07-20 11:17:15 -0400

    Lesson 22 complete.

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,5 +1,9 @@
-import Page from '../components/Page';
+import CreateProduct from '../components/CreateProduct';
 
 export default function SellPage() {
-  return <p>Hello! I am the Sell Page!</p>;
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
