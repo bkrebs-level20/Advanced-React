9724dac05dd2d185a9ad5f4a01c0dc014d1ad3f0
bkrebs-level20 <bkrebs@level20.com>
2022-07-25 15:59:43 -0400

    Module 27 -

--- sick-fits/frontend/pages/product/[id].js
+++ sick-fits/frontend/pages/product/[id].js
@@ -1,7 +1,8 @@
 // Sqaure bracket name is similar for next S.
 // we want it to use a query peram called ID that we canuse to lookup the itme in the db.
 // this will be every product with an [id] in it just from naming conventions in NextJS.
+import SingleProduct from '../../components/SingleProduct';
 
-export default function SingleProduct({ query }) {
-  return <p>Hey imma single product {query.id}</p>;
+export default function SingleProductPage({ query }) {
+  return <SingleProduct id={query.id} />;
 }
