cf8bb1c800534b23d53305a3a757231b24cfc143
bkrebs-level20 <bkrebs@level20.com>
2022-08-05 10:13:21 -0400

    Module 29

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useForm(initial = {}) {
   // create a state object for our inputs(fields)
   const [inputs, setInputs] = useState(initial);
 
-  // Visualization of what our sate will be in the handle change function:
-  /* {
-     name: 'wes',
-     description: 'nice shoes',
-     price: 1000,
-   }
-  */
+  // So for update, this doesn't work great, because the iniital load happens on the server waiting on the data.
+  // but on the client, we have the loading state... it loads for a quick sec then runs it.
+
+  // We are going to do useEffect - to moniotr the variables and when they changa run some code.
+  // Like if we're subscribing to a DB or API and need to send something with data is changed.
+  // useState will actually create an infinite loop because it keeps replacing the data.
+  // So we need something to watch that can trigger the change.  Watch values on object from nothing to something.
+  const initialValues = Object.values(initial).join(''); // values would be the object, and the join would do a string.
+
+  useEffect(() => {
+    // this function runs when the things we are watching change.
+    setInputs(initial);
+  }, [initialValues]);
 
   function handleChange(e) {
     let { value, name, type } = e.target;
